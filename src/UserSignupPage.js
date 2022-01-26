6f33c9c8ce869ceff23caaa9de44b9fafe29975d
yabdioglu <yabdioglu37@gmail.com>
2022-01-26 21:04:04 +0300

    0202-layout

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-class UserSignupPage extends React.Component{
-    render(){
+class UserSignupPage extends React.Component {
+    render() {
         return (
-            <h1>Sign Up</h1>
+            <form>
+                <h1>Sign Up</h1>
+                <div>
+                    <label>Username</label>
+                    <input />
+                </div>
+                <div>
+                    <label>Display Name</label>
+                    <input />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password"/>
+                </div>
+                <div>
+                    <label>Password Repeat</label>
+                    <input type="password"/>
+                </div>
+                <button>Sign Up</button>
+            </form>
         );
     }
 }
