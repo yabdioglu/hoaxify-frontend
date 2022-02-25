ffbd77171125ac19bd164e87e1248610fe4b549a
yabdioglu <yabdioglu37@gmail.com>
2022-02-25 16:24:50 +0300

    0211-progress-indicator

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -6,13 +6,12 @@ class UserSignupPage extends React.Component {
         username: null,
         displayName: null,
         password: null,
-        passwordRepeat: null
+        passwordRepeat: null,
+        pendingApiCall: false
     };
 
     onChange = event => {
         const { name, value } = event.target;
-        // const value = event.target.value;
-        // const name = event.target.name;
         this.setState({
             [name]: value
         })
@@ -28,8 +27,14 @@ class UserSignupPage extends React.Component {
             displayName,
             password
         };
+        this.setState({ pendingApiCall: true });
 
-        axios.post('/api/1.0/users', body);
+        axios.post('/api/1.0/users', body)
+            .then((response) => {
+                this.setState({ pendingApiCall: false });
+            }).catch((error) => {
+                this.setState({ pendingApiCall: false });
+            });
     };
 
     render() {
@@ -54,8 +59,10 @@ class UserSignupPage extends React.Component {
                         <input className="form-control" name="passwordRepeat" type="password" onChange={this.onChange} />
                     </div>
                     <div className="text-center">
-                        <button className="btn btn-primary" onClick={this.onClickSignup}>
-                            Sign Up
+                        <button className="btn btn-primary"
+                            onClick={this.onClickSignup}
+                            disabled={this.state.pendingApiCall}>
+                            {this.state.pendingApiCall && <span className="spinner-border spinner-border-sm"></span>}Sign Up
                         </button>
                     </div>
                 </form>
