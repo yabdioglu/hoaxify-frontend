d5f4ad299c94365ce7642ffc95c0feee61a55bb7
yabdioglu <yabdioglu37@gmail.com>
2022-02-27 22:07:59 +0300

    0511-login-error-on-client

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -6,36 +6,50 @@ import { login } from '../api/apiCalls';
 class LoginPage extends Component {
     state = {
         username: null,
-        password: null
+        password: null,
+        error: null
     }
 
     onChange = event => {
         const { name, value } = event.target;
         this.setState({
-            [name]: value
+            [name]: value,
+            error: null
         });
     }
 
-    onClickLogin = event => {
+    onClickLogin = async event => {
         event.preventDefault();
         const { username, password } = this.state;
         const creds = {
             username,
             password
+        };
+        this.setState({
+            error: null
+        })
+        try {
+            await login(creds);
+        } catch (apiError) {
+            this.setState({
+                error: apiError.response.data.message
+            })
         }
-        login(creds);
     }
 
     render() {
-        const { t } = this.props
+        const { t } = this.props;
+        const { username, password, error} = this.state;
+        const buttonEnabled = username && password;
         return (
-            <div className='container'>
+            <div className="container">
                 <form>
                     <h1 className='text-center'>{t('Login')}</h1>
                     <Input label={t("Username")} name="username" onChange={this.onChange} />
                     <Input label={t("Password")} name="password" type="password" onChange={this.onChange} />
+                    {error && <div className="alert alert-danger">{error}</div>}
                     <div className='text-center'>
-                        <button className='btn btn-primary' onClick={this.onClickLogin}>{t('Login')}</button>
+                        <button disabled={!buttonEnabled} className='btn btn-primary' onClick={this.onClickLogin}>{t('Login')}</button>
                     </div>
                 </form>
             </div>
