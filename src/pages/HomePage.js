928cc36595ad34cd89139637fad75ba4b310e5f3
yabdioglu <yabdioglu37@gmail.com>
2022-03-02 15:44:02 +0300

    0702-react-router

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HomePage() {
     return (
-        <div className='container'>
+        <div className="container">
             homepage
         </div>
     )
