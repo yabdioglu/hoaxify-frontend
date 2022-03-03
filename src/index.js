2c9d7f14bb38997cc344ff90fa1ffc7ff307c5ef
yabdioglu <yabdioglu37@gmail.com>
2022-03-03 20:38:35 +0300

    0806-redux-initial-impl

--- src/index.js
+++ src/index.js
@@ -6,12 +6,38 @@ import reportWebVitals from './reportWebVitals';
 import './i18n';
 import App from './container/App';
 // import AuthenticationContext from './shared/AuthenticationContext';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const loggedInState = {
+  isLoggedIn: true,
+  username: 'user1',
+  displayName: 'display1',
+  image: null,
+  password: 'P4ssword'
+}
+const defaultState = {
+  isLoggedIn: false,
+  username: undefined,
+  displayName: undefined,
+  image: undefined,
+  password: undefined
+}
+
+const reducer = (state, action) => {
+  if (action.type === 'logout-success') {
+    return defaultState;
+  }
+  return state;
+}
+
+const store = createStore(reducer, loggedInState); // İkinci parametre başlangıç state'imiz
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <AuthenticationContext> */}
-    <App />
-    {/* </AuthenticationContext> */}
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
