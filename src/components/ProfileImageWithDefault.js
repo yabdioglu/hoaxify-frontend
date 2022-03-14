750a03e982b7a578158c121ddcda1d63ab84d2af
yabdioglu <yabdioglu37@gmail.com>
2022-03-15 01:32:03 +0300

    1305-serving-image

--- src/components/ProfileImageWithDefault.js
+++ src/components/ProfileImageWithDefault.js
@@ -2,14 +2,15 @@ import React from 'react'
 import defaultPicture from '../assets/profile.png';
 
 const ProfileImageWithDefault = (props) => {
-
-    const { image } = props;
+    const { image, tempimage } = props;
 
     let imageSource = defaultPicture;
     if (image) {
-        imageSource = image;
+        imageSource = 'images/' + image;
     }
-    return <img alt={`Profile`} src={imageSource} {...props} />
+    return <img alt={`Profile`} src={tempimage || imageSource} {...props} onError={(event) => {
+        event.target.src = defaultPicture;
+    }} />
 };
 
 export default ProfileImageWithDefault;
