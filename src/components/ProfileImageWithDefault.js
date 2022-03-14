6f54131574b56c0d42514da0691e5dd962ea970a
yabdioglu <yabdioglu37@gmail.com>
2022-03-15 00:22:09 +0300

    1303-unknown-property-warning

--- src/components/ProfileImageWithDefault.js
+++ src/components/ProfileImageWithDefault.js
@@ -3,13 +3,13 @@ import defaultPicture from '../assets/profile.png';
 
 const ProfileImageWithDefault = (props) => {
 
-    const { image, tempImage } = props;
+    const { image } = props;
 
     let imageSource = defaultPicture;
     if (image) {
         imageSource = image;
     }
-    return <img alt={`Profile`} src={tempImage || imageSource} {...props} />
+    return <img alt={`Profile`} src={imageSource} {...props} />
 };
 
 export default ProfileImageWithDefault;
