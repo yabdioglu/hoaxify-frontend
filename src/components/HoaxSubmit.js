e1642d7a47c946755516dc580beeb9e5ada2f634
yabdioglu <yabdioglu37@gmail.com>
2022-03-21 15:35:34 +0300

    1505-validation

--- src/components/HoaxSubmit.js
+++ src/components/HoaxSubmit.js
@@ -8,13 +8,19 @@ export default function HoaxSubmit() {
     const { image } = useSelector((store) => ({ image: store.image }));
     const [focused, setFocused] = useState(false);
     const [hoax, setHoax] = useState('');
+    const [errors, setErrors] = useState({});
     const { t } = useTranslation();
 
     useEffect(() => {
         if (!focused) {
             setHoax('');
+            setErrors({});
         }
     }, [focused]);
+
+    useEffect(() => {
+        setErrors({});
+    }, [hoax]);
 
     const onClickHoaxify = async () => {
         const body = {
@@ -24,17 +30,27 @@ export default function HoaxSubmit() {
         try {
             await postHoax(body);
             setFocused(false);
-        } catch (error) { }
+        } catch (error) {
+            if (error.response.data.validationErrors) {
+                setErrors(error.response.data.validationErrors);
+            }
+        }
+    }
+
+    let textAreaClass = 'form-control';
+    if (errors.content) {
+        textAreaClass += ' is-invalid';
     }
 
     return (
         <div className='card p-1 flex-row'>
             <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle me-1" />
             <div className='flex-fill'>
-                <textarea className='form-control' rows={focused ? "3" : "1"} onFocus={() => setFocused(true)}
+                <textarea className={textAreaClass} rows={focused ? "3" : "1"} onFocus={() => setFocused(true)}
                     onChange={(event) => setHoax(event.target.value)}
                     value={hoax}
                 />
+                <div className="invalid-feedback">{errors.content}</div>
                 {focused && <div className='text-end mt-1'>
                     <button className='btn btn-primary' onClick={onClickHoaxify}>Hoaxify</button>
                     <button className='btn btn-light d-inline-flex ms-1' onClick={() => setFocused(false)}>
