e9f55962cac40a145bd7936c35a2dec087b05230
yabdioglu <yabdioglu37@gmail.com>
2022-03-22 17:29:03 +0300

    1704-hoax-view-with-user

--- src/components/HoaxView.js
+++ src/components/HoaxView.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import ProfileImageWithDefault from '../components/ProfileImageWithDefault';
+import { Link } from 'react-router-dom';
 
 export default function HoaxView(props) {
     const { hoax } = props;
+    const { user, content } = hoax;
+    const { username, displayName, image } = user;
     return (
-        <div className='card p-1'>{hoax.content}</div>
-    )
-}
+        <div className='card p-1'>
+            <div className='d-flex'>
+                <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle m-1" />
+                <div className='flex-fill m-auto ps-2'>
+                    <Link to={`/user/${username}`} className="text-dark" style={{ textDecoration: 'none' }}>
+                        <h6>
+                            {displayName}@{username}
+                        </h6>
+                    </Link>
+                </div>
+            </div>
+            <div className='ps-5'>
+                {content}
+            </div>
+        </div>
+    );
+};
